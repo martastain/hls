91adf3909f02e4e0e96f386149dae88b2caa7708
Martastain <martas@imm.cz>
2017-03-12 17:11:46 +0100

    manifest class clean-up

--- hls/manifest.py
+++ hls/manifest.py
@@ -1,16 +1,23 @@
+from .protocol import *
 from .segment import HLSSegment
 from .parser import parse_hls
 
 __all__ = ["HLSManifest"]
 
+def mk_tag(key, value=False):
+    result = key
+    if value:
+        result += ":" + str(value)
+    return result + "\n"
+
+
 class HLSManifest():
     def __init__(self, **kwargs):
         self.path_template = kwargs.get("path_template", "/media/{number}.ts")
-        self.hls_version = 3
+        self.hls_version = kwargs.get("version", 3)
+        self.url = kwargs.get("url", "/")
         self.segments = []
         self.duration = 0
-        self.url = kwargs.get("url", "/")
-
         if kwargs.get("parse", False):
             parse_hls(self, kwargs["parse"])
 
@@ -18,12 +25,11 @@ class HLSManifest():
         segment.clip_time = self.duration
         self.segments.append(segment)
         self.duration += segment.duration
+        return segment
 
     @property
     def media_sequence(self):
-        if not self.segments:
-            return 0
-        return self.segments[0].media_sequence
+        return self.segments[0].media_sequence if self.segments else 0
 
     @property
     def target_duration(self):
@@ -34,16 +40,15 @@ class HLSManifest():
 
     def render(self):
         result = "#EXTM3U\n"
-        result+= "#EXT-X-VERSION:{}\n".format(self.hls_version)
-        result+= "#EXT-X-MEDIA-SEQUENCE:{}\n".format(self.media_sequence)
-        result+= "#EXT-X-TARGETDURATION:{}\n".format(self.target_duration)
+        result+= mk_tag(ext_x_version, self.hls_version)
+        result+= mk_tag(ext_x_media_sequence, self.media_sequence)
+        result+= mk_tag(ext_x_targetduration, self.target_duration)
         for i, segment in enumerate(self.segments):
             if segment.is_first:
-                result += "#EXT-X-DISCONTINUITY\n"
+                result += mk_tag(ext_x_discontinuity)
             result += "#EXTINF:{},\n".format(segment.duration)
             result += self.path_template.format(
                     number=self.media_sequence + i
                     )
             result += "\n"
         return result
-
